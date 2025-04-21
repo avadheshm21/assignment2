376a57818c085ac1cdec244afebc238ce2484038
avadheshm21 <mandlik.mandlik@gmail.com>
2025-04-21 17:56:36 +0530

    Add files via upload

---
+++ a2task1.py
@@ -0,0 +1,12 @@
+#Assignment 2
+
+#Task 1: Check if a Number is Even or Odd
+
+a= int(input('Enter a number: '))
+
+if a%2==0:
+    print(a, 'is an Even Number')
+else:
+    print(a, 'is an Odd Number')
+
+
